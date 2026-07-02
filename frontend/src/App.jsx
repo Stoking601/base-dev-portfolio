0bfe24c4d01d0d1999f46b9b48cf71d3ae8196fa
Stoking601 <stoking001@gmail.com>
2026-07-02 16:18:55 +0700

    feat: display token supply and wallet balance

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import { BrowserProvider, Contract } from "ethers";
 import MyToken from "./abi/MyToken.json";
 import { CONTRACT_ADDRESS } from "./config";
+import { formatUnits } from "ethers";
 
 function App() {
   const [account, setAccount] = useState("");
   const [name, setName] = useState("");
   const [symbol, setSymbol] = useState("");
+  const [totalSupply, setTotalSupply] = useState("");
+  const [balance, setBalance] = useState("");
 
   // ⭐ ต้องอยู่นอก connectWallet
   async function loadContractData() {
@@ -20,9 +23,11 @@ function App() {
 
     const tokenName = await contract.name();
     const tokenSymbol = await contract.symbol();
+    const supply = await contract.totalSupply();
 
     setName(tokenName);
     setSymbol(tokenSymbol);
+    setTotalSupply(formatUnits(supply, 18));
   }
 
   async function connectWallet() {
@@ -38,6 +43,21 @@ function App() {
     setAccount(accounts[0]);
 
     await loadContractData(); // ✅ ถูกแล้ว
+    await loadBalance(accounts[0]); // ⭐ เพิ่มตรงนี้
+  }
+
+  async function loadBalance(userAddress) {
+    const provider = new BrowserProvider(window.ethereum);
+
+    const contract = new Contract(
+      CONTRACT_ADDRESS,
+      MyToken.abi,
+      provider
+    );
+
+    const bal = await contract.balanceOf(userAddress);
+
+    setBalance(formatUnits(bal, 18));
   }
 
   return (
@@ -56,6 +76,13 @@ function App() {
           <h3>Token Info</h3>
           <p>Name: {name}</p>
           <p>Symbol: {symbol}</p>
+          <h3>Token Info</h3>
+          <p>Name: {name}</p>
+          <p>Symbol: {symbol}</p>
+          <p>Total Supply: {totalSupply}</p>
+
+          <h3>My Balance</h3>
+          <p>{balance}</p>
         </>
       )}
     </div>
