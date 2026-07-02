c22540052195a506287606b9759ee31cffc11439
Stoking601 <stoking001@gmail.com>
2026-07-02 09:27:00 +0700

    feat: add token metadata variables

---
+++ contracts/MyToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract MyToken {
+
+    string public message = "Hello Base";
+
+    string public name = "Base Learning Token";
+
+    string public symbol = "BLT";
+
+}
