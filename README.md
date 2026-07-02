2edb7f980aaf5d6244df3d16dfb0b050a9597302
Stoking601 <stoking001@gmail.com>
2026-07-02 14:52:03 +0700

    feat: verify MyToken contract on BaseScan

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Network: Base Sepolia
 
 Contract Address:
 0x5a5bb7B1eE8c0fD3209cC24A6546F7337E039458
+
+## Verified Contract
+
+Network: Base Sepolia
+
+Contract:
+0x5a5bb7B1eE8c0fD3209cC24A6546F7337E039458
+
+Status:
+Verified ✅
--- hardhat.config.js
+++ hardhat.config.js
@@ -3,10 +3,15 @@ require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.24",
+
   networks: {
     baseSepolia: {
       url: process.env.BASE_SEPOLIA_RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
