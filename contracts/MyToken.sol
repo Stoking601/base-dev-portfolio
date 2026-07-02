cf2ac4e04ba6e7944ddc1aa8607262307ad57bc8
Stoking601 <stoking001@gmail.com>
2026-07-02 09:45:13 +0700

    feat: add setMessage and resetMessage functions

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.24;
 contract MyToken {
 
     string public message;
+
     string public name;
     string public symbol;
 
@@ -14,6 +15,16 @@ contract MyToken {
         message = "Hello Base";
         name = _name;
         symbol = _symbol;
+    }
+
+    // 🔥 Function 1: เปลี่ยน message
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+
+    // 🔄 Function 2: reset message
+    function resetMessage() public {
+        message = "Hello Base";
     }
 
 }
